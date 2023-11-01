7003583b9ac7b2256d63d8f7cb8ab315a7f19c1a
camilaDiToro <camiladitoro@gmail.com>
2023-11-01 10:25:06 -0300

    Fix idx value

--- visualizations/tau-error/velocity-vs-time-comp.py
+++ visualizations/tau-error/velocity-vs-time-comp.py
@@ -1,9 +1,9 @@
 import json
-
+import os
 from matplotlib import pyplot as plt
 
 prefix = '../../'
-file = 'compVelocity-10-8.80-9.90.json'
+file = 'compVelocity-10-9.80-11.90.json'
 
 fig, ax = plt.subplots()
 
@@ -21,18 +21,19 @@ for v in json_input['velocities']:
     vExp.append(v['vExp'])
     vSim.append(v['vSim'])
 
-
+#vSim[-1] = vSim[-1] - (vSim[-1] - vExp[-1])/2
 
 ax.plot(time, vExp,   label=f'vExp - id={str(json_input["id"])}', marker='o')
 ax.plot(time, vSim,   label=f'vSim - id={str(json_input["id"])}')
-ax.axhline(y=1.6, color='red', linestyle='--', label=r'$v^{max}_d$')
+ax.axhline(y=1.91, color='red', linestyle='--', label=r'$v^{max}_d$')
 
 ax.set_xlabel(r'Tiempo $[s]$')
 ax.set_ylabel(r'Velocidad $[\frac{m}{s}]$')
 ax.grid(False)
 plt.legend()
 
-nombre_de_archivo = f'tp5-velocity-comp-id-{json_input["id"]}.png'
+file_name = os.path.splitext(file)[0]
+nombre_de_archivo = f'tp5-velocity-comp-id-{file_name}.png'
 
 plt.savefig(nombre_de_archivo)
 
